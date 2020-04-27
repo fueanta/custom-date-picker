7093a62984af63653760ad146d44276e81bc1713
Hasnat Jamil <hasnatjamil2018@gmail.com>
2020-04-28 00:47:40 +0600

    React implementation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import DatePicker from "./Components/DatePicker/DatePicker";
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <DatePicker />
+      </div>
+    );
+  }
+}
+
+export default App;
